d178f69155d694f78587823e555a1ef0229c89df
Антон Оленин <giendo1523@yandex.ru>
2025-06-10 23:24:46 +0300

    module2-task1

--- js/functions.js
+++ js/functions.js
@@ -1,57 +1,51 @@
 function isValidLength(inputString, maxLength) {
   return inputString.length <= maxLength;
 }
-console.log(isValidLength("JarvisVerse", 12));
-console.log(isValidLength("Hello, world!", 5));
-
+console.log(isValidLength('JarvisVerse', 12));
+console.log(isValidLength('Hello, world!', 5));
 
 function isPalindrome(str) {
-  // 1. Нормализуем строку: убираем пробелы и приводим к нижнему регистру
-  const normalizedStr = str.replaceAll(' ', '').toLowerCase();
+const normalizedStr = str.replaceAll(' ', '').toLowerCase();
+let reversedStr = '';
 
-  // 2. Создаём пустую строку для перевёрнутой версии
-  let reversedStr = '';
-
-  // 3. Проходим по строке с конца к началу
-  for (let i = normalizedStr.length - 1; i >= 0; i--) {
+for (let i = normalizedStr.length - 1; i >= 0; i--) {
     reversedStr += normalizedStr[i];
   }
-
-  // 4. Сравниваем нормализованную строку и перевёрнутую
-  return normalizedStr === reversedStr;
+return normalizedStr === reversedStr;
 }
+
+
 console.log(isPalindrome('Marvel')); // false
 console.log(isPalindrome('Лёша на полке клопа нашёл')); // true
 console.log(isPalindrome('JarvisVerse')); // false
 
 
-function extractNumber(str) {
-  // Преобразуем входной параметр в строку, на случай, если передано число
-  str = str.toString();
 
+function extractNumber(str) {
+  str = str.toString();
   let result = '';
 
-  // Проходим по каждому символу строки
-  for (let i = 0; i < str.length; i++) {
-    // Пробуем преобразовать символ в число
-    let digit = parseInt(str[i]);
 
-    // Если преобразование успешно (это действительно цифра)
+  for (let i = 0; i < str.length; i++) {
+    const digit = parseInt(str[i], 10);
+
+
     if (!Number.isNaN(digit)) {
-      result += digit; // Добавляем цифру к результату
+      result += digit;
     }
   }
 
-  // Если не нашли ни одной цифры, возвращаем NaN
+
   if (result === '') {
     return NaN;
   }
 
-  // Возвращаем итоговое число
-  return parseInt(result);
+  return parseInt(result, 10);
 }
-console.log(extractNumber('2023 год'));            // 2023
-console.log(extractNumber('ECMAScript 2022'));     // 2022
-console.log(extractNumber('1 кефир, 0.5 батона')); // 105
-console.log(extractNumber('агент 007'));           // 7
-console.log(extractNumber('а я томат'));           // NaN
+
+
+console.log(extractNumber('2023 год'));
+console.log(extractNumber('ECMAScript 2022'));
+console.log(extractNumber('1 кефир, 0.5 батона'));
+console.log(extractNumber('агент 007'));
+console.log(extractNumber('а я томат'));
