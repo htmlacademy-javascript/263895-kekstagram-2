cd1f6b39854bdd696aeb1e60b1c361a1db07d356
Антон Оленин <giendo1523@yandex.ru>
2025-08-06 23:29:20 +0300

    Исправлены ошибки линтера в functions.js

--- js/functions.js
+++ js/functions.js
@@ -14,9 +14,11 @@ function isMeetingInWorkTime(startWork, endWork, startMeeting, duration) {
   return meetingStart >= workStart && meetingEnd <= workEnd;
 }
 
-// Примеры вызова функции для проверки
-console.log(isMeetingInWorkTime('08:00', '17:30', '14:00', 90)); // true
-console.log(isMeetingInWorkTime('8:0', '10:0', '8:0', 120));     // true
-console.log(isMeetingInWorkTime('08:00', '14:30', '14:00', 90)); // false
-console.log(isMeetingInWorkTime('14:00', '17:30', '08:0', 90));  // false
-console.log(isMeetingInWorkTime('8:00', '17:30', '08:00', 900)); // false
+// Примеры вызова функции для проверки (временно отключены для прохождения линтера)
+// console.log(isMeetingInWorkTime('08:00', '17:30', '14:00', 90)); // true
+// console.log(isMeetingInWorkTime('8:0', '10:0', '8:0', 120)); // true
+// console.log(isMeetingInWorkTime('08:00', '14:30', '14:00', 90)); // false
+// console.log(isMeetingInWorkTime('14:00', '17:30', '08:0', 90)); // false
+// console.log(isMeetingInWorkTime('8:00', '17:30', '08:00', 900)); // false
+
+export { isMeetingInWorkTime };
