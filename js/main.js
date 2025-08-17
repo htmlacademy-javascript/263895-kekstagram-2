a66d9ecb45d9ae6ad4ac9ecc44eb20a8a07776b4
Антон Оленин <giendo1523@yandex.ru>
2025-08-17 19:48:53 +0300

    module9-task1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { generatePhotos } from './data.js';
 import { renderThumbnails } from './render-thumbnails.js';
 import { openBigPicture } from './big-picture.js';
+import './form.js';
 
 const photos = generatePhotos(25);
 renderThumbnails(photos, openBigPicture);
